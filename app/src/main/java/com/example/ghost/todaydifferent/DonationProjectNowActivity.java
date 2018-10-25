f9de3eb893bea7cb2aafc0a6eba36ab98a7a8070
charlene <chsmile0310@gmail.com>
2018-10-25 14:45:17 +0200

    features/ donate now page implementation

--- app/src/main/java/com/example/ghost/todaydifferent/DonationProjectNowActivity.java
+++ app/src/main/java/com/example/ghost/todaydifferent/DonationProjectNowActivity.java
@@ -5,43 +5,38 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 
-public class DonationProjectActivity extends Activity {
-    ImageView img_projectImage;
-    Button btn_do_pro_back;
-    Button btn_do_pro_sign;
+public class DonationProjectNowActivity extends Activity {
+    Button btn_do_pj_sign_back;
+    Button btn_do_pj_sign_donate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.act_donationproject);
+        setContentView(R.layout.act_donation_now);
 
-        btn_do_pro_sign = findViewById(R.id.btn_do_pro_sign);
-        btn_do_pro_sign.setOnClickListener(new View.OnClickListener() {
+        btn_do_pj_sign_back = findViewById(R.id.btn_do_pj_sign_back);
+        btn_do_pj_sign_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
                 Intent intent = new Intent();
-                intent.setClass(DonationProjectActivity.this, DonationProjectNowActivity.class);
+                intent.setClass(DonationProjectNowActivity.this, DonationProjectActivity.class);
                 startActivity(intent);
                 finish();
             }
         });
 
-        btn_do_pro_back = findViewById(R.id.btn_do_pro_back);
-        btn_do_pro_back.setOnClickListener(new View.OnClickListener() {
+        btn_do_pj_sign_donate = findViewById(R.id.btn_do_pj_sign_donate);
+        btn_do_pj_sign_donate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
                 Intent intent = new Intent();
-                intent.setClass(DonationProjectActivity.this, DonationActivity.class);
+                intent.setClass(DonationProjectNowActivity.this, DonationThanksActivity.class);
                 startActivity(intent);
                 finish();
             }
         });
-
-
     }
 }
-
