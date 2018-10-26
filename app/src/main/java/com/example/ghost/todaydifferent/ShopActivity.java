30e7fbefd35989bb7f6b91181c526646b5f2540a
charlene <chsmile0310@gmail.com>
2018-10-26 23:48:41 +0200

    fix/ fix the detailed UI of shop page

--- app/src/main/java/com/example/ghost/todaydifferent/ShopActivity.java
+++ app/src/main/java/com/example/ghost/todaydifferent/ShopActivity.java
@@ -10,27 +10,11 @@ import android.widget.Spinner;
 
 public class ShopActivity extends Activity {
     ImageButton btn_shop_back;
-    Spinner spinner_type;
-    Spinner spinner_filter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_shop);
-
-        spinner_type = findViewById(R.id.spinner_type);
-        ArrayAdapter<CharSequence> typeList = ArrayAdapter.createFromResource(ShopActivity.this,
-                R.array.shop_type_list,
-                android.R.layout.simple_spinner_dropdown_item);
-        spinner_type.setAdapter(typeList);
-
-
-        spinner_filter = findViewById(R.id.spinner_filter);
-        ArrayAdapter<CharSequence> filterList = ArrayAdapter.createFromResource(ShopActivity.this,
-                R.array.shop_filter_list,
-                android.R.layout.simple_spinner_dropdown_item);
-        spinner_filter.setAdapter(filterList);
-
 
         btn_shop_back = findViewById(R.id.btn_shop_back);
         btn_shop_back.setOnClickListener(new View.OnClickListener() {
@@ -44,8 +28,5 @@ public class ShopActivity extends Activity {
                 finish();
             }
         });
-
-
     }
-
 }
