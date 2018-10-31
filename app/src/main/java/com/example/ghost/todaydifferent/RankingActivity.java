679db04d8fc45944b6818dc657920497555d8edd
charlene <chsmile0310@gmail.com>
2018-10-31 03:16:53 +0100

    refactor/ make user-related specific word in ranking page more flixable

--- app/src/main/java/com/example/ghost/todaydifferent/RankingActivity.java
+++ app/src/main/java/com/example/ghost/todaydifferent/RankingActivity.java
@@ -5,14 +5,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class RankingActivity extends Activity {
     ImageButton btn_rank_back;
+    ImageView rank_user_img;
+    TextView rank_user_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_ranking);
+
+        final GlobalVariable gv = (GlobalVariable)getApplicationContext();
 
         btn_rank_back = findViewById(R.id.btn_rank_back);
         btn_rank_back.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +32,13 @@ public class RankingActivity extends Activity {
                 finish();
             }
         });
+
+        rank_user_img = findViewById(R.id.rank_user_img);
+        rank_user_img.setImageResource(((gv.getUserName() == getResources().getString(R.string.user_lukas)) ? R.drawable.lukas_img : R.drawable.amy_img));
+
+        rank_user_name = findViewById(R.id.rank_user_name);
+        rank_user_name.setText((gv.getUserName() == getResources().getString(R.string.user_lukas)) ? R.string.user_lukas : R.string.user_amy);
+
 
 
     }
